11ade4fb88ffd605c7f3702e59a925e871a3ff2e
Andrei Yangabishev <waffleboot@gmail.com>
2020-09-18 20:43:49 +0300

    add main

--- chapter3/main.go
+++ chapter3/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -13,4 +17,13 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	pod, err := clientset.CoreV1().Pods("book").Get(context.Background(), "example", metav1.GetOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(pod.Status.PodIP)
 }
